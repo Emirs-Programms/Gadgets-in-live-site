38302d0ae60278783be789d7a50df2f35d50e3b5
Your Name <emirmarash@gmail.com>
2025-04-10 09:33:41 +0600

    update commit2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 // Components
 import Header from './components/header/Header';
